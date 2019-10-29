f79c056a27552f92657bde8e1af8a1cd58ee0ce8
valentina3101 <valentinacisneros156@gmail.com>
2019-10-28 21:27:42 -0300

    actualizando!

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
@@ -16,3 +18,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
